0c7c95ea4b1e1694e9fe67873a2407421d30db70
Gerasko-Vadim <vadim.gerasko99@gmail.com>
2021-05-06 15:17:30 +0600

    add endpoints the get all list groups

--- src/groups/groups.controller.ts
+++ src/groups/groups.controller.ts
@@ -1,5 +1,13 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
+import { GroupsService } from "./groups.service";
+import { IGroup } from "./interface/group.interface";
 
 @Controller('groups')
 export class GroupsController {
+    constructor(private readonly groupsService: GroupsService) { }
+
+    @Get()
+    async signInUser(): Promise<IGroup[]>{
+        return this.groupsService.findAll()
+    }
 }
