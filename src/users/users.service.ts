1b3dc976846b34f54c944109962500c37c45d044
Gerasko-Vadim <vadim.gerasko99@gmail.com>
2021-05-09 16:47:35 +0600

    fixed

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -39,13 +39,10 @@ export class UsersService {
 
     async findByToken(req: any): Promise<IUsers> {
         const token = req.headers.authorization.slice(7);
-        const tokenExist = await this.tokenService.verifyToken(token)
-        console.log(tokenExist)
-        if(tokenExist){
-            const user = await this.find(tokenExist._id)
-            return user;
-        }
-  
+        const { _id } = await this.tokenService.verifyToken(token)
+        return this.find(_id);
+
+
     }
 
     async getAllTeachers(): Promise<IUsers[]> {
