35517127e762a9cea5137c37d57911a24e8d2fe7
Gerasko-Vadim <vadim.gerasko99@gmail.com>
2021-05-21 17:01:49 +0600

    fixed bug the update users

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.enableCors();
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3005);
 }
 bootstrap();
--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -2,8 +2,6 @@ import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 import { IsFile } from "nestjs-form-data";
 
 export class UpdateUser{
-    @IsString()
-    id:string
 
     @IsString()
     name: string
